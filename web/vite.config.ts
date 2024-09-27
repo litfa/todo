c0d99db50c99a5c8e068b81d0bb4d35df0d1b0b5
ltfei <ltfei@ltfei.cn>
2024-09-27 08:37:29 +0800

    chore: 调整依赖关系

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-import.d.ts',
+      resolvers: [AntDesignVueResolver()]
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver({ importStyle: false, resolveIcons: true })]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
