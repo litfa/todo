cd30f7818eabab90b603a2da699c6d4d798797c1
ltfei <ltfei@ltfei.cn>
2024-09-29 13:47:52 +0800

    feat(web): pinia 数据持久化

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { persistedstate } from '@/utils/localforage'
 
 import App from './App.vue'
 import router from './router'
@@ -7,8 +8,11 @@ import router from './router'
 import './assets/style/main.less'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(persistedstate)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
---
+++ web/src/utils/localforage.ts
@@ -0,0 +1,16 @@
+import type { PiniaPluginContext } from 'pinia'
+import localforage from 'localforage'
+
+const prefix = 'todo_'
+const getStoreKey = (storeId: string) => `${prefix}${storeId}`
+
+export function persistedstate({ pinia }: PiniaPluginContext) {
+  pinia.use(async ({ store }) => {
+    const data = await localforage.getItem(getStoreKey(store.$id))
+    store.$state = data as {}
+
+    store.$subscribe((e, state) => {
+      localforage.setItem(getStoreKey(e.storeId), JSON.parse(JSON.stringify(state)))
+    })
+  })
+}
