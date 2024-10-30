96ffa6e1a5999ba399d2735d125425fe4ecf6191
ltfei <ltfei@ltfei.cn>
2024-10-30 23:01:42 +0800

    feat(web): 默认任务列表

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { inbox } from '@ltfei/todo-common'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,11 +8,12 @@ const router = createRouter({
       path: '/',
       name: 'layout',
       component: () => import('@/layout/Default.vue'),
+      redirect: `/tasks/${inbox}`,
       children: [
-        {
-          path: 'tasks',
-          component: () => import('../views/Tasks/Tasks.vue')
-        },
+        // {
+        //   path: 'tasks',
+        //   component: () => import('../views/Tasks/Tasks.vue')
+        // },
         {
           path: 'tasks/:id',
           component: () => import('../views/Tasks/Tasks.vue')
