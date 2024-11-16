f5bc358cc1ec4ace1f13086b3366ace714a13363
ltfei <ltfei@ltfei.cn>
2024-11-16 23:08:30 +0800

    feat: push失败日志输出

--- service/packages/utils/handlingCommits.ts
+++ service/packages/utils/handlingCommits.ts
@@ -14,6 +14,7 @@ import { generateId, generateIdString, parse10RadixId } from '@/utils/snowflake'
 import { convertKeysToSnakeCase, convertKeysToCamelCase } from './camelToSnakeCase'
 import cronParser from 'cron-parser'
 import { CamelObjectKeys } from '@/db/types'
+import { app as logger } from '@/utils/log'
 
 type Table = SubTasksTable | TaskListTable | TasksTable
 
@@ -79,6 +80,7 @@ const handlingCommit = async (commit: Commit, t?: Transaction): Promise<Result> 
     }
     await t.commit()
   } catch (e) {
+    logger.error(e)
     await t.rollback()
     err = true
   }
