241bb3b522a4b0c0fde14c927ebc3fe614c71be7
ltfei <ltfei@ltfei.cn>
2025-04-02 09:49:32 +0800

    chore(website): 网站首页

---
+++ website/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'App'
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="less" scoped></style>
---
+++ website/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'HomeView'
+})
+</script>
+
+<template></template>
+
+<style lang="less" scoped></style>
