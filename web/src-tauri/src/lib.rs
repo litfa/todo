e47902cbd7829a03d1dae770e89c96f4f5ce258f
ltfei <ltfei@ltfei.cn>
2025-04-11 07:57:22 +0800

    chore(web): deep-link

--- web/src-tauri/src/lib.rs
+++ web/src-tauri/src/lib.rs
@@ -3,6 +3,7 @@
 
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
