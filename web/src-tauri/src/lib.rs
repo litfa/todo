be8fdecb550d2aa274665439574df22be554e1c2
ltfei <ltfei@ltfei.cn>
2025-04-11 08:55:34 +0800

    chore(web): 单例

--- web/src-tauri/src/lib.rs
+++ web/src-tauri/src/lib.rs
@@ -1,15 +1,20 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-// mod tray;
+mod tray;
+
+use tauri::{AppHandle, Manager};
 
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = show_window(app);
+        }))
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
-                // tray::create_tray(app.handle())?;
+                tray::create_tray(app.handle())?;
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
@@ -20,4 +25,16 @@ pub fn run() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn show_window(app: &AppHandle) {
+    let window = app.get_webview_window("todo").unwrap();
+
+    window
+        .show()
+        .expect("Can't bring window to focus");
+
+    window
+        .set_focus()
+        .expect("Can't bring window to focus");
 }
